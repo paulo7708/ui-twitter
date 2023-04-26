a6f1770fec773506d35822932f5dbf76a9072f2d
Paulo7708 <dawnbreakerph@gmail.com>
2023-04-26 12:36:29 -0300

    Corrigido Bug

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -8,34 +8,33 @@ interface TweetProps {
 export const Tweet = (props: TweetProps) => {
   return (
     <>
-      <div className="container">
-        <a href="#" className="tweet">
+      <a href="#" className="tweet">
           <img src="https://github.com/paulo7708.png" alt="Paulo" />
-        </a>
-        <div className="tweet-content">
-          <div className="tweet-content-header">
-            <strong>Paulo Henrique</strong>
-            <span> @paulo7708</span>
-          </div>
-          
-          <p>{props.content}</p>
 
-          <div className="tweet-content-footer">
-            <button type="button">
-              <ChatCircle />
-              20
-            </button>
-            <button type="button">
-              <ArrowsClockwise />
-              20
-            </button>
-            <button type="button">
-              <Heart />
-              20
-            </button>
+          <div className="tweet-content">
+            <div className="tweet-content-header">
+              <strong>Paulo Henrique</strong>
+              <span> @paulo7708</span>
+            </div>
+
+            <p>{props.content}</p>
+
+            <div className="tweet-content-footer">
+              <button type="button">
+                <ChatCircle />
+                20
+              </button>
+              <button type="button">
+                <ArrowsClockwise />
+                20
+              </button>
+              <button type="button">
+                <Heart />
+                20
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
+        </a>
     </>
   );
 };
