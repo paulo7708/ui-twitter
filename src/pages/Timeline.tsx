75050b5ac8e1770f862320b39491ff1b15780c53
Paulo7708 <dawnbreakerph@gmail.com>
2023-04-27 12:25:51 -0300

    useState

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,25 +1,44 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/header/header";
 import { Separator } from "../components/separator/separator";
 import { Tweet } from "../components/tweets/Tweet";
 
-const tweets = ["Meu primeito Tweet", "Deu certo", "Teste 3"];
+import "./timeline.css"
+
 
 export const Timeline = () => {
+  const [ tweets, setTweets ] = useState(["Meu primeito Tweet", "Deu certo", "Teste 3"])
+  let [ newTweet, setNewTweet ] = useState('') // controlled Components
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+    
+    setTweets(prevTweets => [newTweet,...prevTweets])
+    setNewTweet('')
+  }
+
+
+
   return(
     <main className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/paulo7708.png" alt="img" />
-          <textarea id="tweet" placeholder="What's happening?"></textarea>
+          <textarea
+          id="tweet"
+          placeholder="What's happening?"
+          onChange={(event) => {
+            setNewTweet(event.target.value)
+          }}
+          value={newTweet}
+          />
         </label>
         <button type="submit">Tweet</button>
       </form>
 
       <Separator />
-
-      <Tweet content="tweet1" />
 
       {tweets.map((tweet: string) => {
         return <Tweet key={tweet} content={tweet} />;
